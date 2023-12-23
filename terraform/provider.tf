c6293c78239cd2d4e8120f03c8762898ed037ddf
mgarber93 <matthewgarber@proton.me>
2023-12-22 19:33:02 -0500

    Remove unneeded keys

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-  region     = var.AWS_REGION
+  region = "us-east-1"
 }
